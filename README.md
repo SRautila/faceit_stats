1c5a7152325cc9126e2f98102aabedd6778a6672
Sebastian Rautila <sebastianrautila@gmail.com>
2016-09-06 14:36:03 +0200

    code cleanup, tried to make redraw event fire only once

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# faceit_leauges
-Chrome extension which shows both of your faceit-leauges and ranks on your profile.
+# faceit_stats
+Chrome extension which adds a stats table to your match lobby and shows both of your faceit-leauges and ranks on your profile.
 
 Installation: Download the source code, enable Developer Mode in chrome://extensions/ and add the source code folder by clicking "Load unpacked extension...".
-Installation alternative: Add the .crx-release to Chrome.
--- js/showLeague.js
+++ js/showLeague.js
@@ -2,18 +2,12 @@
  * Created by Sebastian on 2016-08-24.
  */
 
-document.addEventListener('DOMSubtreeModified', function() {
-    if(document.getElementsByClassName("page-title__content__title__primary page-title__content__title__primary--text-transform-none")[0].textContent.length > 0){
-        updateStats()
-    }
-});
-
 function updateStats() {
 
-    var statsFind = new XMLHttpRequest();
-    statsFind.onload = function () {
+    var userStats = new XMLHttpRequest();
+    userStats.onload = function () {
 
-        var json = JSON.parse(statsFind.responseText);
+        var json = JSON.parse(userStats.responseText);
         var leagues = json.payload.leagues;
 
         var leagueOut = leagues[0].rank_label + " / " + leagues[1].rank_label;
@@ -38,8 +32,8 @@ function updateStats() {
 
             if (user.name = userName) {
                 var statsData = user.guid + "/games/csgo/league?region=EU";
-                statsFind.open('GET', "https://api.faceit.com/api/users/" + statsData);
-                statsFind.send();
+                userStats.open('GET', "https://api.faceit.com/api/users/" + statsData);
+                userStats.send();
             }
         });
     };
@@ -50,3 +44,16 @@ function updateStats() {
     userFind.send();
 
 }
+
+var draw = function () {
+
+    var elem = document.getElementsByClassName("stat__detail__value")[3];
+    if (typeof elem !== 'undefined') {
+        if (elem.textContent.length > 0) {
+            updateStats();
+            document.removeEventListener('DOMSubtreeModified', draw, false);
+        }
+    }
+};
+
+document.addEventListener('DOMSubtreeModified', draw, false);
